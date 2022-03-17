b4ede034c711a240470b8a198b0082e7b1394b9b
Suzy Clark <suzyclark2019@gmail.com>
2022-03-17 19:20:24 -0400

    Finishing test_get_all_movies

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,4 +1,20 @@
 # TODO: Feature 1
+from src.repositories.movie_repository import MovieRepository
+from src.models.movie import Movie
+
 def test_get_all_movies():
-    #get_all_movies()
-    assert 3+4 == 5
+    movie_repo = MovieRepository()
+    movie1 = Movie("Last of the Mohicans", "Michael Mann", 8)
+    movie2 = Movie("No Country for Old Men", "Joel and Ethan Coen", 8)
+    movie3 = Movie("Dunghole the Movie", "Micheal Bay", 7) #this one is an inside joke
+    movie4 = Movie("Dunkirk", "Christopher Nolan", 8)
+    movie_repo._db.append(movie1)
+    movie_repo._db.append(movie2)
+    movie_repo._db.append(movie3)
+    movie_repo._db.append(movie4)
+    movie_list = movie_repo.get_all_movies()
+    assert movie_list[0] == movie1
+    assert movie_list[1] == movie2
+    assert movie_list[2] == movie3
+    assert movie_list[3] == movie4
+    
