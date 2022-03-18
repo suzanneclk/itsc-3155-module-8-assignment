2b31bde2bac53fc7aa76d4712a961c7e229a7faa
Kyla Moore <kylamoore@MooreBook-Pro.local>
2022-03-17 22:09:33 -0400

    Third commit

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,16 @@
 # TODO: Feature 2
+from src.repositories.movie_repository import MovieRepository
+from src.models.movie import Movie
+
+def test_create_movie():
+    movie_repo = MovieRepository()
+    movie = movie_repo.create_movie("It Follows", "David Robert Mitchell", 7)
+    assert type(movie) == Movie
+    assert movie.title == "It Follows"
+    assert movie.rating == 7
+    assert movie.director == "David Robert Mitchell"
+    movie = movie_repo.create_movie("The Babadook", "Jennifer Kent", 7)
+    assert type(movie) == Movie
+    assert movie.title == "The Babadook"
+    assert movie.rating == 7
+    assert movie.director == "Jennifer Kent"
